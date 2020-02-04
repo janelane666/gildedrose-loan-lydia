d0cc701ff482dd7642cebfbfe81b614b2c953160
Jane Lane <loan.does@gmail.com>
2020-02-04 15:54:38 +0100

    test ok

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,30 +5,22 @@ from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
     #test -1 jour
-    def test_cake_valide(self):
-        items = [Item("Conjured Mana Cake", 3, 6)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual(5, items[0].quality)
+    def test_gr(self):
 
-    #test produit périmé
-    def test_cake_perime(self):
-        items = [Item("Conjured Mana Cake", -1, 6)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual(4, items[0].quality)
+    	items = [
+    		Item("Aged Brie", 2, 0),
+    		Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      		Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+      		Item("Conjured Mana Cake", 3, 6)
+      		]
 
-    def test_brie_present(self):
-        items = [Item("Aged Brie", -5, 1)]  
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual(3, items[0].quality)
-
-    def test_brie(self):
-        items = [Item("Aged Brie", 1, 0)]  
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual(1, items[0].quality)
+    	for i in range(30):
+        	gilded_rose = GildedRose(items)
+        	gilded_rose.update_quality()
+        	print()
+        	print("Nous sommes le jour", i, ":")
+        	for x in range(len(items)):
+        		print("Le produit", items[x].name, "vaut", items[x].quality,".")
 
 if __name__ == '__main__':
     unittest.main()
